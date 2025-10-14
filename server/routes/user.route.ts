1d9b22442355553e8c044e0ecc3392071eb28f11
ahmadfiqrioemry <foemry@gmail.com>
2025-10-15 00:09:52 +0700

    chore : add update endpoint

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
-import { UserController } from "@/controllers/user.controller";
 import { adminOnly, authMiddleware } from "@/middleware/auth";
+import { UserController } from "@/controllers/user.controller";
 
 const user = new Hono();
 user.use(authMiddleware);
@@ -8,8 +8,6 @@ user.put("/profile", UserController.updateProfile);
 
 user.use(adminOnly);
 user.get("", UserController.getUsers);
-
-// TODO : implement these routes
 user.post("/create", UserController.createUsers);
 user.put("/:id/update", UserController.updateUsers);
 user.patch("/:id/role", UserController.changeRole);
