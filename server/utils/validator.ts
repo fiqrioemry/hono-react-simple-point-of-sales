3264a9497f0dc83eae61a4bb3d92932c6bfcfebd
ahmadfiqrioemry <foemry@gmail.com>
2025-10-14 23:37:27 +0700

    chore: remove validate for json request

--- server/utils/validator.ts
+++ server/utils/validator.ts
@@ -1,16 +1,5 @@
 import { z } from "zod";
-import { ZodSchema } from "zod";
 import { Context } from "hono";
-
-export const validate = (schema: ZodSchema, data: any) => {
-  const result = schema.safeParse(data);
-
-  if (result.error) {
-    throw result.error;
-  }
-
-  return result.data;
-};
 
 export async function parseForm<T>(
   c: Context,
