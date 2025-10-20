3fe79f3bbc5ea8d0d769022ff5d6a78047055113
fiqrioemry <foemry@gmail.com>
2025-10-20 22:42:06 +0700

    feat(add): outlet services

---
+++ server/services/outlet.service.ts
@@ -0,0 +1,11 @@
+export class OutletServices {
+  static async getAll() {}
+
+  static async getById(id: string) {}
+
+  static async create(req: any) {}
+
+  static async update(req: any) {}
+
+  static async delete(id: string) {}
+}
