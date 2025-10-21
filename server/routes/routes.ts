95f83cb6c5d122c3eb8785d462ad1ea2e97d8d8d
fiqrioemry <foemry@gmail.com>
2025-10-21 23:53:19 +0700

    feat(add): api route and new controllers outlet

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -7,5 +7,22 @@ import products from "./product.route";
 
 const router = new Hono();
 
+router.get("/", (c) => {
+  return c.redirect("/health");
+});
+
+router.get("/health", (c) => {
+  return c.json({
+    status: "OK",
+    message: "condition healthy",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+router.route("/api/v1/auth", auth);
+router.route("/api/v1/users", user);
+router.route("/api/v1/products", products);
+router.route("/api/v1/categories", category);
+router.route("/api/v1/outlets", outlet);
 
 export default router;
