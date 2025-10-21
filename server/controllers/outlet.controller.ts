d6f0f1f8307e9b3dae6a373b3f304dc905e69efc
fiqrioemry <foemry@gmail.com>
2025-10-21 23:54:07 +0700

    update change outlet controller

--- server/controllers/outlet.controller.ts
+++ server/controllers/outlet.controller.ts
@@ -11,16 +11,3 @@ export class OutletController {
 
   static async deleteOutlet(c: Context) {}
 }
-import { Context } from "hono";
-
-export class OutletController {
-  static async getAllOutlets(c: Context) {}
-
-  static async getOutletById(c: Context) {}
-
-  static async createOutlet(c: Context) {}
-
-  static async updateOutlet(c: Context) {}
-
-  static async deleteOutlet(c: Context) {}
-}
