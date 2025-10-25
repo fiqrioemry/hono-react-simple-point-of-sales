828d5a9dc92d064d842bad71d6fc85dcc708327f
fiqrioemry <foemry@gmail.com>
2025-10-25 23:46:50 +0700

    feat(add) : new controller' '

---
+++ server/controllers/test.controller.ts
@@ -0,0 +1,13 @@
+import { Context } from "hono";
+
+export class OutletController {
+  static async getAllOutlets(c: Context) {}
+
+  static async getOutletById(c: Context) {}
+
+  static async createOutlet(c: Context) {}
+
+  static async updateOutlet(c: Context) {}
+
+  static async deleteOutlet(c: Context) {}
+}
